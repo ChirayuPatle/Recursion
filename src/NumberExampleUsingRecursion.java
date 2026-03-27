357605f84d58099c574794714241c07151c5d537
Chirayupatle <chirayu.patle@vectorlab.in>
2026-03-27 21:30:06 +0530

    learn: recursion

--- src/NumberExampleUsingRecursion.java
+++ src/NumberExampleUsingRecursion.java
@@ -4,11 +4,11 @@ public class NumberExampleUsingRecursion {
         number(1);
     }
     static void number(int n){
+        // base condition
         if(n > 5){
             return;
         }
         System.out.println(n);
         number(n + 1);
-
     }
 }
